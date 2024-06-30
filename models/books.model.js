71a8e77a4ff159f55de21345b0eb652488a7e795
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-06-30 16:58:27 +0530

    books updation,deletion and filtering functionality is done

--- models/books.model.js
+++ models/books.model.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema({
     title: {type:String, required: true},
     cost: {type:String, required: true},
     totalPages: {type:String, required: true},
+    status: {type: String, default:"pending"},
     userId: {type: String, required: true},
     userName: {type:String, required: true}
 })
--- routes/book.route.js
+++ routes/book.route.js
@@ -16,8 +16,13 @@ bookRouter.post("/create", auth, async (req,res)=>{
 })
 
 bookRouter.get("/all", async (req,res)=>{
+    const {status, title } = req.query;
+    const filter = {};
+    if(status) filter.status = new RegExp(status, "i");
+    if(title) filter.title = new RegExp(title, "i");
+
     try {
-        const allBooks = await BookModel.find();
+        const allBooks = await BookModel.find(filter);
         res.send(allBooks);
     } catch (error) {
         console.log(error);
@@ -40,6 +45,50 @@ bookRouter.get("/mybooks", auth, async (req,res)=>{
     }
 })
 
+
+bookRouter.patch("/update/:bookId", auth, async (req,res)=>{
+    const updatedData = req.body;
+    const {bookId} = req.params;
+    const {userId} = req.body;
+    try {
+        const book = await BookModel.findOne({_id:bookId});
+        console.log(book);
+        if(book){
+            if(book.userId == userId){
+                await BookModel.findByIdAndUpdate({_id: bookId}, {$set: {...updatedData}});
+                res.send({msg: "book updated."})
+            }else{
+                res.send({msg: "you are not authorized to update this book"});
+            }
+        }else{
+            res.send("book not found")
+        }
+    } catch (error) {
+        console.log(error);
+        res.send({msg: "error occured"});
+    }
+})
+
+bookRouter.delete("/delete/:bookId", auth, async (req,res)=>{
+    const {userId} = req.body;
+    const {bookId} = req.params;
+    try {
+        const book = await BookModel.findOne({_id:bookId});
+        if(book){
+            if(book.userId == userId){
+                await BookModel.findByIdAndDelete({_id: bookId});
+                res.send({msg: "book deleted."})
+            }else{
+                res.send({msg: "you are not authorized to delete this book"});
+            }
+        }else{
+            res.send("book not found");
+        }
+    } catch (error) {
+        console.log(error);
+        res.send({msg: "error occured"});
+    }
+})
 
 
 module.exports = bookRouter;
